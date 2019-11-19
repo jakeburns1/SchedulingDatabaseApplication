0a383f3a48dad9f676faad6697209be5f5ef6cf2
Gunner Gunner <gunner.gunner@centre.edu>
2019-11-18 23:27:25 -0500

    Minimalist Proctors

---
+++ html/proctors.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+     <title>Proctor Home</title>
+  </head>
+  <body>
+    <hr><b>Proctor Home Page</b></hr>
+    <?php echo '<p> not yet connected to database, so cannot display views</p>'; ?>
+  </body>
+</html>
