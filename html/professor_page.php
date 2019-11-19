d0ec137f39a03ebb6e4170650d0c495367ab4e5d
Jiayu Jiang <jiayu.jiang@centre.edu>
2019-11-18 23:36:50 -0500

    Update professor_page.php

--- html/professor_page.php
+++ html/professor_page.php
@@ -5,19 +5,19 @@
 	</head>
 	
 	<body>
-	  <h1 align = 'center'> Your Test Schedule </h1>
-	  <table style="width:100%">
-        <tr>
-            <th>Course</th>
-            <th>Test Time</th>
-            <th>Test Location</th>
-        </tr>
-        <tr>
-            <td>CSC410</td>
-            <td>2:20-3:50</td>
-            <td>Centre College</td>
-        </tr>
-</table>
+	    <h1 align = 'center'> Your Test Schedule </h1>
+	    <table style="width:100%">
+            <tr>
+                <th>Course</th>
+                <th>Test Time</th>
+                <th>Test Location</th>
+            </tr>
+            <tr>
+                <td>CSC410</td>
+                <td>2:20-3:50</td>
+                <td>Centre College</td>
+            </tr>
+        </table>
 	  
 	<?php ?>
 	</body>
