954f967a86084c47dab1067fac55f28327acd257
Jake T. Burns <jake.burns@centre.edu>
2019-11-19 03:41:15 +0000

    initial php page

---
+++ html/test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<title>Achme Test Log In</title>
+	</head>
+	<body>
+	  <h1 align = 'center'> Please log in below </h1>
+
+	  <form align = 'center'>
+	    username: <input type = 'text' name = 'profLogin'> <br>
+	    password: <input type = 'password' name = 'profPass'><br>
+
+	    <button type="button">login as professor</button>
+	    <button type="button">login as proctor</button>
+	    </form>
+	<?php ?>
+	</body>
+	</html>
