27f500ad5a1c7f7dfc3f6255396450806cfae9f9
Gunner Gunner <gunner.gunner@centre.edu>
2019-11-25 23:01:36 -0500

    Update proctors.php

--- html/proctors.php
+++ html/proctors.php
@@ -18,7 +18,7 @@ DEPENDENCIES: none
            font-size:300%;
         }
         table{
-           border-collapse: collapse;
+           border-collapse: thick;
         }
     </style>
   </head>
@@ -26,31 +26,95 @@ DEPENDENCIES: none
        <h1 align='center'> <b>Welcome Back</b> </h1>
 
        <h2 align ='center'>Upcoming exams</h2>
-        <?php
-           $pdo = connect_to_psql('acme_proctoring');
-           $sql = <<<'SQL'
-		SELECT name, test_date, test_time, 
-		       CASE WHEN  test_isPaper = TRUE THEN 'yes'
-			    ELSE 'no' 
-		       END As paper, 
-                       professor_name  
-                  FROM tests_information
+       <?php
+
+           function displayTests($pdo)
+	   {
+             $sql = <<<'SQL'
+		     SELECT name, test_date, test_time, test_id, 
+                            class,
+		            CASE WHEN  test_isPaper = TRUE THEN 'yes'
+			         ELSE 'no' 
+		            END As paper, 
+                            professor_name  
+                       FROM tests_information
 
 SQL;
-	   $stmt = $pdo->query($sql);
-	   echo "<p><table border = 1, align ='center'>";
-	   echo"<tr><th>student name</th><th>test date</th><th>test time</th><th>paper copy</th><th>assigned by</th></tr>";
-	   while($row = $stmt->fetch())     
-	  {
+	      try
+	      {
+	       $stmt = $pdo->query($sql);
+	      }
+	      catch(\PDOException $e)
+	      {
+               debug_message('Error Occured: '.$e);
+	      }
+	      echo "<p><table border = 1, align ='center'>";
+	      echo"<tr><th>Student Name</th>";
+	      echo"<th>Class</th>";
+	      echo"<th>Assigned By</th>";
+	      echo"<th>Test Date</th>";
+	      echo"<th>Test Time</th>";
+	      echo"<th>Paper Copy</th>";
+	      echo"<th> </th><th>  </th><th> </th><th> </th></tr>";
+
+	      echo"<form method = 'post'>";
+	      $index = 0;
+	      while($row = $stmt->fetch())     
+	      {
+		  $ids = $row['test_id'];
+//		  var_dump($ids);
 		  echo"<tr>\n";
 		  echo"<td>". $row['name'] . "</td>\n";
+		  echo"<td>". $row['class'] . "</td>\n";
+                  echo"<td>". $row['professor_name'] . "</td>\n";
 		  echo"<td>". $row['test_date'] . "</td>\n";
 		  echo"<td>". $row['test_time'] . "</td>\n";
 		  echo"<td>". $row['paper'] . "</td>\n";
-		  echo"<td>". $row['professor_name'] . "</td>\n";
+         //	  echo"<td>" . submitButton('delete[' . $id . ']', 'Delete') . '</td>' . "\n";
+//		  echo"<td>" . submitButton('edit[' . $id . ']', 'Edit') . '</td>' . "\n";
+		  echo"<td>". "<input type='submit' name = 'start[".$ids."]' value = 'START'>" . "</td>\n";
+		  echo"<td>". "<input type='submit' name = 'end[".$ids."]' value = 'END'>" . "</td>\n";
+
+                  echo"<td>". "<input type='submit' name = 'edit[".$ids."]' value = 'EDIT'>" . "</td>\n";
+		  echo"<td>". "<input type='submit' name = 'delete[".$ids."]' value = 'DELETE'>".
+			      "<input type = 'hidden' name = 'test_id[".$ids."]' value = $ids>". "</td>\n" ;
+		  echo"</tr>";
+                  $index++;
+	      }
+	      echo"</form>";
+	      return $pdo;
+	   }
+       function main()
+	   {
+             var_dump($_POST);
+	     $pdo = connect_to_psql('acme_proctoring');
+	//     $edit = process_post($pdo);
+//	     if($edit===true)
+//	     {
+               if(isset($_POST['edit']))
+	       {
+		       echo"<p>you tried to edit a test</p>";
+		  $action = $_POST['edit'];
+	          foreach($_POST['test_id'] as $num)
+		  {
+	           try
+		   {
+		     echo"<p>you tried to edit".$action[$num];
+		   }
+		   catch(\PDOException $e){debug_message("ERROR: ".$e);}
+		  }
+                  echo"<UL><a href = 'proctors.php'>return</a></UL>";
+	       }
 
 
-          }
+//	     }
+	     else
+	     {
+	        $pdo = displayTests($pdo);
+	     }
+	   }
+
+	   main();
        ?> 
     </body>
 
