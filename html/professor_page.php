e87eacbf252efc4bf185da0c9c8cd8e5e28f6596
Jiayu Jiang <jiayu.jiang@centre.edu>
2019-11-25 21:50:47 -0500

    Update professor_page.php

--- html/professor_page.php
+++ html/professor_page.php
@@ -4,7 +4,7 @@ require('professor_page_functions.php');
 
 /*  Queries the view display_ducks and displays the results as a table. */
 function display_tests($pdo) {
-  $sql = 'SELECT * FROM tests_information';
+  $sql = 'SELECT * FROM professor_test';
   $data = $pdo->query($sql);
   
   echo '<h1>Test Schedule</h1>';
@@ -14,10 +14,10 @@ function display_tests($pdo) {
   echo '<tr><th>Course</th><th>Student</th><th>Test Time</th><th></th>Test Location<th></th><th></th></tr>';
   foreach ($data as $row) {
 	echo '<tr>';
-    echo '<td></td>';
-	echo '<td></td>';
-	echo '<td>' . $row['test_time'] . '</td>';
-	echo '<td>' . $row['test_location'] . '</td>';
+    echo '<td>' . $row['course'] . '</td>';
+	echo '<td>' . $row['name'] . '</td>';
+	echo '<td>' . $row['time'] . '</td>';
+	echo '<td>' . $row['location'] . '</td>';
 	echo '<td><input type="submit" name= "delete" value="Delete" /></td>';
 	echo '<td><input type="submit" name= "edit" value="Edit" /></td>';
 	echo '</tr>';
