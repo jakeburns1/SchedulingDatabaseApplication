fc9aaf41cbe26e766046f07db3801e7792a55182
Jake T. Burns <jake.burns@centre.edu>
2019-12-02 13:28:21 -0500

    inserting hashed account into new users table

--- html/test.php
+++ html/test.php
@@ -22,7 +22,7 @@
 	       
 	       $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 	       
-	       var_dump($hashed_password);
+	       $insertAccountSQL = 'INSERT INTO users ';
 
 	     ?>
 	</body>
