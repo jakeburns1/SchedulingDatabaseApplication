b4034f9af9a17041a2a0b8d7ddb687c930b47aa8
Jiayu Jiang <jiayu.jiang@centre.edu>
2019-12-02 21:10:20 -0500

    Update professor_page.php

--- html/professor_page.php
+++ html/professor_page.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require('professor_page_functions.php');
+require('html_professor_page_functions.php');
 
 $pdo = connect_to_psql('project', $verbose=TRUE);
 
@@ -20,13 +20,7 @@ if (isset($_POST['edit'])) {
     echo "<input type='text' name='test_date' />";
 	
     echo "<p>Test Start Time:</p>";
-	echo "<select name='start_time'><option selected>Choose one</option>";
-	$start_time_array = array("08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00",);
-	foreach ($start_time_array as $time) {
-	echo "<option value='" . $row['time'] . "'>" . $row['time'] . "</option>";
-	}
-	echo "</select><br><br>";
-	
+echo "<input type='text' name='start_time' />";
     echo "<p>Test University:</p>";
     echo "<select name='university'><option selected>Choose one</option>";
 	$sql = 'SELECT DISTINCT university FROM tests';
@@ -41,7 +35,7 @@ if (isset($_POST['edit'])) {
 
 if (isset($_POST['update'])) {
 	$sql = 'UPDATE students_tests SET test_date = :test_date, 
-	test_start_time = :test_start_time
+	test_start_time = :test_start_time, 
 	WHERE student_id = :student_id';
 	$stmt = $pdo->prepare($sql);
 
@@ -54,7 +48,7 @@ if (isset($_POST['update'])) {
 				     
 
 
-/*  Queries the view display_ducks and displays the results as a table. */
+/*  Queries the view display_tests and displays the results as a table. */
 function display_tests($pdo) {
   $sql = 'SELECT * FROM professor_test';
   $data = $pdo->query($sql);
