1fcc83dc022d79c548a3645773bb05e6ea1dbd16
Alexander A. Garuba <alexander.garuba@centre.edu>
2019-12-03 23:43:00 -0500

    Update professor_page.php

--- html/professor_page.php
+++ html/professor_page.php
@@ -92,7 +92,7 @@ function display_tests($pdo) {
 		 debug_message("Error: ".$e);
 	}
 	
-	$result = $stmt->fetch_all(PDO::FETCH_ASSOC);
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
   
   echo '<h1 align="center">Test Schedule</h1>';
   
